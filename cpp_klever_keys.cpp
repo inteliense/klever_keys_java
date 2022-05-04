3404b770a9489ec61701c57decf3493daed404fa
Ryan Fitzgerald <101024833+inteliense@users.noreply.github.com>
2022-05-04 03:01:48 -0400

    Update cpp_klever_keys.cpp

--- cpp_klever_keys.cpp
+++ cpp_klever_keys.cpp
@@ -6,10 +6,6 @@
 extern "C" void get_key_c(const char* input, char* output) {
 
     std::string result = KleverKeys::get(input);
-    printf("%s\n",result.c_str());
-    //output = (char*) malloc(65);
     strcpy(output, result.c_str());
-    //output[64] = '\0';
-    printf("%s\n", output);
 
 }
