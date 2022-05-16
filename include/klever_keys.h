c2b3c5392a429e6d331e5f62f9a7ff1b8a372810
Ryan Fitzgerald <101024833+inteliense@users.noreply.github.com>
2022-05-16 15:15:10 -0400

    Update klever_keys.h

--- include/klever_keys.h
+++ include/klever_keys.h
@@ -5,6 +5,7 @@
 #ifndef KLEVER_KEYS_C_EXEC_KLEVER_KEYS_H
 #define KLEVER_KEYS_C_EXEC_KLEVER_KEYS_H
 
-#endif //KLEVER_KEYS_C_EXEC_KLEVER_KEYS_H
-
 extern char* get_key(const char* input);
+
+
+#endif //KLEVER_KEYS_C_EXEC_KLEVER_KEYS_H
